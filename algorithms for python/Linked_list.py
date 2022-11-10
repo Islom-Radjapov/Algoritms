c0914481a5cc9e262a4e8d8e5e724cf8f0b2f9c8
Islom-Radjapov <radjapov_islom@mail.com>
2022-11-10 19:38:55 +0500

    learning Linked list

---
+++ algorithms for python/Linked_list.py
@@ -0,0 +1,45 @@
+class Node:
+    """Tugun (node) obyekt"""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LindekList:
+    """Linked list obyekti"""
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+    def push(self, new_data):
+        """List boshiga yangi element qoshish"""
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+
+
+llist = LindekList()
+llist.head = Node("Dushanba")
+tuesday = Node("Seshanba")
+wednesday = Node("Chorshanba")
+
+llist.head.next = tuesday
+tuesday.next = wednesday
+llist.push("Yakshanba")
+
+llist.printList()
+
+
+"""                      Linked listlar 3hil turda boladi
+        1. SINGLY Linked list = Bir tomonlami royhat
+        2. CICRULAR Linked list = Aylanma royhat
+        3. DOUBLE Lindek list = Ikki tomonlami royhat
+
+        Afzalliklari tez yozish va tez o'chirish yana 1 ta operatsiya blan bajariladi O(1)
+        Kamchiliklari sekin qidirish va har bir tugun xotiradi koproq joy egallaydi   O(n)
+"""

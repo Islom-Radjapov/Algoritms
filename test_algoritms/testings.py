d7373f2f49591f634071d27885d427dde58c638d
islom <radjapov_islom@mail.ru>
2022-11-13 02:24:46 +0500

    testing algo

--- test_algoritms/testings.py
+++ test_algoritms/testings.py
@@ -2,15 +2,11 @@ from datetime import datetime as date
 
 def numJewelsInStones( jewels, stones):
     start = date.now().time()
-    print( start.microsecond)
     result = 0
-    for x in jewels:
-        for y in stones:
-            if x == y:
-                result += 1
+    for x in range(9999999):
+        x
     end = date.now().time()
     print( f"Search completed in {end.second - start.second} seconds {end.microsecond - start.microsecond} microseconds" )
-    return result
 
 jewels = "aA"
 stones = "aAAbbbb"

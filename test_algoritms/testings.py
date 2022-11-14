3571237e2bf200acde169ceacf0242fde2582bd4
islom <radjapov_islom@mail.ru>
2022-11-14 20:25:58 +0500

    testing algo

--- test_algoritms/testings.py
+++ test_algoritms/testings.py
@@ -11,9 +11,37 @@ def test():
 
     # test 2
     start = date.now().time()
-    [x + 5 for x in range(99999999)]
+    [x + 5 for x in range(99999999)].count()
+
     end = date.now().time()
     print(f"Search completed in {end.second - start.second} seconds {end.microsecond - start.microsecond} microseconds")
 
 
-test()
+# test()
+
+def my_strlen(param_1):
+    x = 0
+    try:
+        while param_1[x] != 0:
+            x += 1
+        return x
+    except:
+        return x
+
+# print( my_strlen("RaInB0w d4Sh!") )
+
+
+
+def my_strrchr(param_1, param_2):
+    x = len(param_1) - 1
+    while x > 0:
+        if param_1[x] == param_2:
+            return param_1[x:]
+        x -= 1
+
+
+print( my_strrchr("abcabc", "b") )
+
+print( my_strrchr( "121212", "2") )
+
+print( my_strrchr("abc", "d" ) )

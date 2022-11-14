169198a26a2f08f5c1d7fba3a72d9a7d013b20b5
islom <radjapov_islom@mail.ru>
2022-11-15 00:39:01 +0500

    testing algo

--- test_algoritms/testings.py
+++ test_algoritms/testings.py
@@ -46,63 +46,76 @@ def my_strrchr(param_1, param_2):
 # print( my_strrchr( "121212", "2") )
 #
 # print( my_strrchr("abc", "d" ) )
+# def test_null():
+#     res = 0
+#     for string in p1:
+#         for char in string:
+#             for rand in p1:
+#                 if string == rand:
+#                     continue
+#                 elif char in rand:
+#                     continue
+#                 else:
+#                     res += 1
+#     # print(res)
+#     return True
+
+
 param8 = ["xoxAoxo", "xoxAox", "oxAox", "oxo", "A", "ooxAoxx", "oxooxo", "Axo"]
+param4 = ["bonjour", "salut", "bonjour", "bonjour"]
 def str_maxlenoc(p1, p2):
-    def test(p1):
-        for string in p1:
-            for char in string:
-                for rand in p1:
-                    if string == rand:
-                        continue
-                    elif char in rand:
-                        continue
-                    else:
-                        return False
-        return True
-    # if test(p1=p1):
-    """ ["x oxAox o", "x oxAox", "oxAox"] """
-    param = p1[0]
-    start = 0
-    end = len(param)
-    res1 = []
-    for p in range( int( len(param) / 2 ) ):
-        for rand in p1[1:]:
-            if param[start:] in rand:
-                print("start", param[start:] )
-                res1.append(param[start:end])
-                continue
-            elif param[:end] in rand:
-                print( "end", param[:end] )
-                # print(end)
-                # print(param[:end])
-                res1.append(param[start:end])
-                continue
-            else:
-                res1 = []
-                break
-        if len(res1) == p2:
-            return res1
-        start += 1
-        end -= 1
-    return
+    def test_null():
+        res = 0
+        for string in p1[1:]:
+            for char in p1[1]:
+                if char in string:
+                    res += 1
+                    continue
+        print("ssss", res)
+
+    test_null()
+
+    def test_string():
+        param = p1[0]
+        start = 0
+        end = len(param)
+        res1 = []
+        for p in range( int( len(param) / 2 ) ):
+            for rand in p1[1:]:
+                if param[start:end] in rand:
+                    res1.append(param[start:end])
+                    continue
+                else:
+                    res1 = []
+                    break
+            if len(res1) == p2 - 1:
+                return res1
+            start += 1
+            end -= 1
+        return False
+
+    # if test_null():
+    #     # print("bit")
+    #     if test_string():
+    #         # print("bir")
+    #         return test_string()
+    #     else:
+    #         # print("222")
+    #         for x in p1[1:]:
+    #             for y in range(len(p1[0])):
+    #                 if p1[0][y] in x:
+    #                     if p1[0][y:] in x:
+    #                         return p1[0][y:]
+    #                     return p1[0][y]
 
 
 
-        # for x in p1[1:]:
-        #     for y in range(len(p1[0])):
-        #         if p1[0][y] in x:
-        #             if p1[0][y:] in x:
-        #                 return p1[0][y:]
-        #             return p1[0][y]
+# print( str_maxlenoc(["xoxAoxo", "xoxAox", "oxAox"],  3) )
+#
+print( str_maxlenoc(param8, 8) )
 
 
-
-print( str_maxlenoc(["xoxAoxo", "xoxAox", "oxAox"],  3) )
-
-# print( str_maxlenoc(param8, 8) )
-
-
-# print( param8[0].split()[0] in param8[1].split() )
+# print( str_maxlenoc( param4, 4 ) )
 
 
 
